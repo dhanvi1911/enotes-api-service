f97f540934cd3d37a688c29a9990b189a8dfc0f2
Dhanvi <dhanvibhanushali@gmail.com>
2025-07-04 19:04:50 +0530

    Added Files Dto

--- src/main/java/com/dhanvi/enotes_api_service/dto/FileDetailsDto.java
+++ src/main/java/com/dhanvi/enotes_api_service/dto/FileDetailsDto.java
@@ -1,28 +1,27 @@
-//package com.dhanvi.enotes_api_service.dto;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    @Getter
-//    @Setter
-//    @Entity
-//    public class FileDetailsDto {
-//
-//        private Integer id;
-//        private String uploadFileName;
-//        private String originalFilename;
-//        private String displayFileName;
-//        private String path;
-//        private Long fileSize;
-//
-//    }
-//
-//
+package com.dhanvi.enotes_api_service.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public class FileDetailsDto {
+
+        private Integer id;
+        private String uploadFileName;
+        private String originalFilename;
+        private String displayFileName;
+        private String path;
+        private Long fileSize;
+
+    }
+
+
